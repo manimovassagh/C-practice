7d7f0102a631467dc77df770c16cf525d49b1b34
Mani Movassagh <mani.mgh@gmail.com>
2024-03-14 01:22:46 +0100

    define struct

--- Hello-world.c
+++ Hello-world.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+
+
+
+typedef struct{
+char name[20];
+int age;
+float gpa;
+
+} Student;
 
 int main()
 {
     printf("Hello C language again\n");
     printf("Hello C language");
+    //print sample student
+    Student s1;
+    s1.age = 20;
+    s1.gpa = 3.5;
+    strcpy(s1.name, "John");
+    printf("Student name: %s\n", s1.name);
     return 0;
 }
